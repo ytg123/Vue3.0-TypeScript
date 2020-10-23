2131e31caff8e08b1d810a22a9fc83472418206a
yangtengguang <18734153794@163.com>
2020-10-23 08:58:50 +0800

    update eslint

--- terry-zheye/.eslintrc.js
+++ terry-zheye/.eslintrc.js
@@ -14,17 +14,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'prettier/prettier': [
-    //     // eslint校验不成功后，error或2则报错，warn或1则警告，off或0则无提示
-    //     'error',
-    //     {
-    //       // 不要分号
-    //       semi: false,
-    //       // 设置单引号
-    //       singleQuote: true,
-    //       // 设置换行长度
-    //       printWidth: 160
-    //     }
-    //   ]
+    'semi': ['error', 'never']
   }
 }
